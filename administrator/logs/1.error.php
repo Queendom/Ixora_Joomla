c7400b481b180fc50975a55ea8c9cabfb732fba1
Queendom <73855171+Queendom@users.noreply.github.com>
2022-10-05 15:29:23 -0400

    Ixora Music Joomla
    Ixora is a fictional music company website that focuses on being a to-go site for album recommendations, latest news, and events. This was done in Joomla for a class final project.

---
+++ administrator/logs/1.error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2022-04-30 02:32:33 UTC
+#Software: Joomla! 4.0.6 Stable [ Furaha ] 15-January-2022 17:06 GMT
+
+#Fields: datetime	priority clientip	category	message
+2022-04-30T02:32:33+00:00	INFO 71.70.174.9	joomlafailure	Username and password do not match or you do not have an account yet.
